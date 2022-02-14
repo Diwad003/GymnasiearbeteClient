1164b7b01274cb49b60cd2a34ed23827dd7ebcbd
Diwad003 <48996015+Diwad003@users.noreply.github.com>
2022-02-14 17:13:30 +0100

    Sending from server to client works
    Next step is processing the data on clientside

--- Gymnasiearbete/Assets/Networking.cs
+++ Gymnasiearbete/Assets/Networking.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -12,9 +13,9 @@ using UnityEngine.UI;
 
 public class Networking : MonoBehaviour
 {
-    public TcpClient myServerSocket = new TcpClient();
+    public Socket myServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
     
-    public TcpClient GetServerSocket()
+    public Socket GetServerSocket()
     {
         return myServerSocket;
     }
@@ -36,11 +37,10 @@ public class Networking : MonoBehaviour
 
             IPAddress tempIPAddress = IPAddress.Parse(tempIP);
             IPEndPoint tempServerEndpoint = new IPEndPoint(tempIPAddress, 1000);
-            myServerSocket = new TcpClient(tempServerEndpoint);
 
             // Connect to Remote EndPoint  
             myServerSocket.Connect(tempServerEndpoint);
-            Debug.Log("Socket connected to " + myServerSocket.Client.RemoteEndPoint.ToString());
+            Debug.Log("Socket connected to " + myServerSocket.RemoteEndPoint.ToString());
 
             GetComponent<UI>().myConnectionMenu.SetActive(false);
         }
@@ -60,42 +60,37 @@ public class Networking : MonoBehaviour
     public List<string> Receive()
     {
         //Get data from server
-        myServerSocket.ReceiveTimeout = 10000;
-        byte[] tempBufferArray = new byte[1024];
-        int tempReceivedBytes = myServerSocket.Client.Receive(tempBufferArray); //tempReceivedBytes gets size of the total send buffer for some reason
-        Debug.Log("Send Size" + tempReceivedBytes);
+        myServerSocket.ReceiveTimeout = 30000;
+        byte[] tempBufferArray = new byte[4];
+        myServerSocket.Send(Encoding.ASCII.GetBytes("Send"));
+        int tempReceivedBytes = myServerSocket.Receive(tempBufferArray);
 
-
-
-        List<byte> tempBuffer = new List<byte>();
-        tempBufferArray = new byte[1024];
-
-        int tempAmountOfBits = myServerSocket.Client.Receive(tempBufferArray);
-        string str = Encoding.ASCII.GetString(tempBufferArray, 0, tempAmountOfBits);
-        Debug.Log("str " + str);
-
-        Debug.Log("tempAmountOfBits " + tempAmountOfBits);
-        for (int j = 0; j < tempAmountOfBits; j++)
+        int tempBufferInt = 0;
+        for (int i = 0; i < tempReceivedBytes; i++)
         {
-            tempBuffer.Add(tempBufferArray[j]);
-            Debug.Log("Buffer Array " + tempBufferArray[j]);
+            if (tempBufferArray[i] != 0)
+            {
+                tempBufferInt = tempBufferArray[i];
+            }
+        }
+
+        
+        for (int i = 0; i < tempBufferInt; i++)
+        {
+            tempReceivedBytes = myServerSocket.Receive(tempBufferArray);
         }
 
 
+        string tempString = Encoding.ASCII.GetString(tempBufferArray);
+        List<string> tempBufferList = tempString.Split('/').ToList();
 
-        //Convert to string and return
-        string tempString = Encoding.UTF8.GetString(tempBuffer.ToArray());
-        for (int i = 0; i < tempString.Length; i++)
+        //if (tempBufferList[0])
         {
-            tempString = tempString.Replace("\0", string.Empty);
+
         }
 
-        string[] tempStringArray = tempString.Split('/');
+
         List<string> tempStringList = new List<string>();
-        for (int i = 0; i < tempStringArray.Length; i++)
-            tempStringList.Add(tempStringArray[i]);
-
-
         GameObject.Find("LastRequestText").GetComponent<Text>().text = tempStringList[0];
         tempStringList.RemoveAt(0);
 
@@ -104,6 +99,6 @@ public class Networking : MonoBehaviour
 
     public void Send(byte[] aBuffer)
     {
-        myServerSocket.Client.Send(aBuffer);
+        myServerSocket.Send(aBuffer);
     }
 }
